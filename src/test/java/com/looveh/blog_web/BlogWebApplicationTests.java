66f51f80770b14bff1f799edc52ff3ca7d39ea9c
卢威 <luwei01@dongfangfuli.com>
2019-06-13 15:00:12 +0800

    fixed

--- src/test/java/com/looveh/blog_web/BlogWebApplicationTests.java
+++ src/test/java/com/looveh/blog_web/BlogWebApplicationTests.java
@@ -9,9 +9,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.List;
-import java.util.Map;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BlogWebApplicationTests {
@@ -25,10 +22,10 @@ public class BlogWebApplicationTests {
     @Test
     public void contextLoads() {
 
-        String sql = "select * from t_user";
-        List<Map<String, Object>> maps = jdbcTemplate.queryForList(sql);
-
-        System.out.println(maps);
+//        String sql = "select * from t_user";
+//        List<Map<String, Object>> maps = jdbcTemplate.queryForList(sql);
+//
+//        System.out.println(maps);
     }
 
     @Test
